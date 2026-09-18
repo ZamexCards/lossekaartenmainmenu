664a1e9f76fa2cf92463350756f6bb3279972f27
ZamexCards <zameykat@gmail.com>
2026-09-18 15:50:11 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.ZAMEX_CONFIG = {
   // Gepubliceerd tabblad "Beheer" uit Google Sheets.
-  sheetCsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSTJceggqgKnKo0_n9m-DSkZ-YpBKDesuClx-BJ0FO1Fz1u3js-z-AG7KeSStJZ9g/pub?gid=1956853300&single=true&output=csv'
+  sheetCsvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSTJceggqgKnKo0_n9m-DSkZ-YpBKDesuClx-BJ0FO1Fz1u3js-z-AG7KeSStJZ9g/pub?gid=630810122&single=true&output=csv'
 };
